afe536b00b70fbe3bcd7d63610ea4ac72d37de77
Paul Nakata <paul@pmn.org>
2013-04-05 13:00:54 -0700

    Game creation

--- js/site.js
+++ js/site.js
@@ -3,43 +3,44 @@ var app = angular.module('app', ['firebase']);
 var firebaseUrl = "http://munchkinscores.firebaseio.com/";
 var munchkinRef = new Firebase(firebaseUrl);
 
-function HomeCtrl($rootScope, $scope, $location, angularFire) {
+function HomeCtrl($scope, $location, angularFire) {
     var url = firebaseUrl + "games";
-    $scope.games = angularFire(url, $scope, 'games');
-
-    $scope.authClient = new FirebaseAuthClient(munchkinRef, function(err, user) {
-      if (user) {
-        $scope.setName(user.email);
-      }
-    });
+    $scope.games = angularFire(url, $scope, 'games', []);
 
     $scope.login = function() {
         // Authorize the user
         $scope.authClient.login('persona');
     };
 
-    $scope.setName = function(username) {
-        // Set the user's name
-        $rootScope.username = username;
-    };
-
     $scope.newgame = function() {
         // Generate a new game. 
         var gameName = generateName();
+        $scope.games.push({name: gameName, created: new Date() });
         $location.path("/g/" + gameName);
     };
 }
 
-
 function GameCtrl($rootScope, $scope, $route, $routeParams, angularFire){
     var url = firebaseUrl + $routeParams.name;
-    $scope.username = $rootScope.username;
 }
-
 
 app.config(['$routeProvider', function ($routeProvider){
     $routeProvider.
         when('/g/:name', { templateUrl: 'tmpl/game.html', controller: GameCtrl }).
         when('/', { templateUrl: 'tmpl/home.html', controller: HomeCtrl }).
         otherwise({ redirectTo: '/' });
+}]);
+
+app.run(['$rootScope', '$location', function (scope, $location){
+    scope.authClient = new FirebaseAuthClient(munchkinRef, function(err, user) {
+      if (user) {
+        // Get the user's username from firebase. If it doesn't exist, allow the user to choose one
+        scope.setName(user.email);
+      }
+    });
+
+    scope.setName = function(username) {
+        // Set the user's name
+        scope.username = username;
+    };
 }]);
